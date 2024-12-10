218768a38f7fd7d8960dfd468a26c8e0df0f6bd2
m1st0rm <mishaboy333@gmail.com>
2024-12-10 23:38:19 +0100

    Added get_students_stats_non_aggregated method in backend/pandas_utils.py and changed methods definition style

--- backend/pandas_utils.py
+++ backend/pandas_utils.py
@@ -1,12 +1,17 @@
 import pandas
-from typing import List
+from typing import List, Dict
 
 
-def read_xlsx(file_path: str) -> pandas.DataFrame:
+def read_xlsx(
+        file_path: str
+) -> pandas.DataFrame:
     return pandas.read_excel(file_path)
 
 
-def map_dfs_columns(dfs: List[pandas.DataFrame], key_column: str = "ФИО") -> List[pandas.DataFrame]:
+def map_dfs_columns(
+        dfs: List[pandas.DataFrame],
+        key_column: str = "ФИО"
+) -> List[pandas.DataFrame]:
     mapped_dfs = []
     for i, df in enumerate(dfs):
         updated_df = df.copy()
@@ -19,9 +24,22 @@ def map_dfs_columns(dfs: List[pandas.DataFrame], key_column: str = "ФИО") -> 
     return mapped_dfs
 
 
-def join_dfs(dfs: List[pandas.DataFrame], join_column: str = "ФИО") -> pandas.DataFrame:
+def join_dfs(
+        dfs: List[pandas.DataFrame],
+        join_column: str = "ФИО"
+) -> pandas.DataFrame:
     result_df = dfs[0]
     for df in dfs[1:]:
         result_df = pandas.merge(result_df, df, on=join_column, how='inner')
 
     return result_df
+
+
+def get_students_stats_non_aggregated(
+        df: pandas.DataFrame,
+        set_index: str = "ФИО"
+) -> List[Dict[str, Dict[str, str | int]]]:
+    dfs_divided_by_full_name = [row.to_frame().T.reset_index(drop=True) for _, row in df.iterrows()]
+    return [df.set_index(set_index).to_dict(orient='index') for df in dfs_divided_by_full_name]
+
+
