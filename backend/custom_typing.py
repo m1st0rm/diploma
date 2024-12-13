c9083feeeaf3b836f3c944d4cc78ec15c2a472b4
m1st0rm <mishaboy333@gmail.com>
2024-12-13 14:35:32 +0100

    Changed type annotation format in backend/pandas_utils.py and backend/custom_typing.py

--- backend/custom_typing.py
+++ backend/custom_typing.py
@@ -1,5 +1,2 @@
-from typing import List, Dict
-
-
-STUDENTS_STATS_NON_AGGREGATED_TYPE = List[Dict[str, Dict[str, str | int]]]
-STUDENTS_STATS_AGGREGATED_BY_CONTROL_FORM_TYPE = List[Dict[str, Dict[str, Dict[str, str | int]]]]
+STUDENTS_STATS_NON_AGGREGATED_TYPE = list[dict[str, dict[str, str | int]]]
+STUDENTS_STATS_AGGREGATED_BY_CONTROL_FORM_TYPE = list[dict[str, dict[str, dict[str, str | int]]]]
--- backend/pandas_utils.py
+++ backend/pandas_utils.py
@@ -1,18 +1,19 @@
 import pandas
-from typing import List, Dict
+from pandas import DataFrame
+from typing import List
 from backend.custom_typing import STUDENTS_STATS_NON_AGGREGATED_TYPE as students_stats_non_aggregated_type
 
 
 def read_xlsx(
         file_path: str
-) -> pandas.DataFrame:
+) -> DataFrame:
     return pandas.read_excel(file_path)
 
 
 def map_dfs_columns(
-        dfs: List[pandas.DataFrame],
+        dfs: list[DataFrame],
         key_column: str = "ФИО"
-) -> List[pandas.DataFrame]:
+) -> list[DataFrame]:
     mapped_dfs = []
     for i, df in enumerate(dfs):
         updated_df = df.copy()
@@ -26,9 +27,9 @@ def map_dfs_columns(
 
 
 def join_dfs(
-        dfs: List[pandas.DataFrame],
+        dfs: list[DataFrame],
         join_column: str = "ФИО"
-) -> pandas.DataFrame:
+) -> DataFrame:
     result_df = dfs[0]
     for df in dfs[1:]:
         result_df = pandas.merge(result_df, df, on=join_column, how='inner')
@@ -37,7 +38,7 @@ def join_dfs(
 
 
 def get_students_stats_non_aggregated(
-        df: pandas.DataFrame,
+        df: DataFrame,
         set_index: str = "ФИО"
 ) -> students_stats_non_aggregated_type:
     dfs_divided_by_full_name = [row.to_frame().T.reset_index(drop=True) for _, row in df.iterrows()]
