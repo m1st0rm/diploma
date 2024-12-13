3c8355c5b59b5532308ce610289112af2a08322b
m1st0rm <mishaboy333@gmail.com>
2024-12-13 22:39:58 +0100

    Updated backend/custom_typing.py

--- backend/custom_typing.py
+++ backend/custom_typing.py
@@ -1,2 +1,1 @@
-STUDENTS_STATS_NON_AGGREGATED_TYPE = list[dict[str, dict[str, str | int]]]
-STUDENTS_STATS_AGGREGATED_BY_CONTROL_FORM_TYPE = list[dict[str, dict[str, dict[str, str | int]]]]
+STUDENTS_STATS_RAW_TYPE = list[dict[str, dict[str, str | int]]]
