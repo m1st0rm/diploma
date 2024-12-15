e6a15ba6fb33fb29b312cea658517c12a2242ddf
m1st0rm <mishaboy333@gmail.com>
2024-12-15 18:24:55 +0100

    Added get_students_stats_with_discipline_configs_grouped_by_category method in backend/data_utils.py and STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_GROUPED_BY_CATEGORY_TYPE, renamed STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS to STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_TYPE in backend/custom_typing.py

--- backend/data_utils.py
+++ backend/data_utils.py
@@ -1,6 +1,7 @@
 from backend.custom_typing import (
     STUDENTS_STATS_RAW_TYPE,
-    STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS
+    STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_TYPE,
+    STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_GROUPED_BY_CATEGORY_TYPE
 )
 from backend.classes.discipline_config import DisciplineConfig
 
@@ -17,7 +18,7 @@ REGULAR_CATEGORY = 'regular'
 
 def get_students_stats_with_discipline_configs(
         students_stats: STUDENTS_STATS_RAW_TYPE
-) -> STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS:
+) -> STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_TYPE:
     students_stats_with_discipline_configs = {}
     for student_stats in students_stats:
         for student_full_name, disciplines_dict in student_stats.items():
@@ -51,3 +52,36 @@ def get_students_stats_with_discipline_configs(
             students_stats_with_discipline_configs[student_full_name] = discipline_configs
 
     return students_stats_with_discipline_configs
+
+
+def get_students_stats_with_discipline_configs_grouped_by_category(
+        students_stats_with_discipline_configs: STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_TYPE
+) -> STUDENTS_STATS_WITH_DISCIPLINE_CONFIGS_GROUPED_BY_CATEGORY_TYPE:
+    students_stats_with_discipline_configs_grouped_by_category = {}
+    categories = (REGULAR_CATEGORY, COURSE_WORK_CATEGORY, COURSE_PROJECT_CATEGORY, PRACTICE_CATEGORY)
+    for student_full_name, stats_discipline_configs in students_stats_with_discipline_configs.items():
+        discipline_configs_grouped_by_category = {
+            REGULAR_CATEGORY: [],
+            COURSE_WORK_CATEGORY: [],
+            COURSE_PROJECT_CATEGORY: [],
+            PRACTICE_CATEGORY: []
+        }
+
+        for stats_discipline_config in stats_discipline_configs:
+            if stats_discipline_config.categoty == PRACTICE_CATEGORY:
+                discipline_configs_grouped_by_category[PRACTICE_CATEGORY].append(stats_discipline_config)
+            elif stats_discipline_config.categoty == COURSE_WORK_CATEGORY:
+                discipline_configs_grouped_by_category[COURSE_WORK_CATEGORY].append(stats_discipline_config)
+            elif stats_discipline_config.categoty == COURSE_PROJECT_CATEGORY:
+                discipline_configs_grouped_by_category[COURSE_PROJECT_CATEGORY].append(stats_discipline_config)
+            else:
+                discipline_configs_grouped_by_category[REGULAR_CATEGORY].append(stats_discipline_config)
+
+        for category in categories:
+            discipline_configs_grouped_by_category[category].sort(
+                key=lambda discipline_config: discipline_config.semester)
+
+        students_stats_with_discipline_configs_grouped_by_category[
+            student_full_name] = discipline_configs_grouped_by_category
+
+    return students_stats_with_discipline_configs_grouped_by_category
