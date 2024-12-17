aa5db466ee59f1407cd4beb592cf7ec8e4ecbbec
m1st0rm <mishaboy333@gmail.com>
2024-12-17 12:11:26 +0100

    Added docstrings in backend/classes/discipline_config.py

--- backend/classes/discipline_config.py
+++ backend/classes/discipline_config.py
@@ -1,8 +1,31 @@
+"""
+Module defining the DisciplineConfig class for storing discipline-specific configurations.
+
+This class represents the structure of discipline data, including its name, semester,
+mark, study hours, credits, and other related attributes.
+
+Classes:
+- DisciplineConfig: A dataclass for managing discipline configurations.
+
+"""
+
 from dataclasses import dataclass
 
 
 @dataclass
 class DisciplineConfig:
+    """
+    A class representing the configuration of a discipline.
+
+    Attributes:
+        contol_form: The form of control for the discipline (e.g., exam, test, project).
+        name: The name of the discipline.
+        semester: The semester number in which the discipline was studied.
+        mark: The mark or grade for the discipline. Can be an integer, string, or list of mixed values.
+        study_hours: The total number of study hours assigned to the discipline.
+        credits_number: The number of credits associated with the discipline (can be integer or float).
+        categoty: The category of the discipline.
+    """
     contol_form: str
     name: str
     semester: int
@@ -12,6 +35,12 @@ class DisciplineConfig:
     categoty: str
 
     def __repr__(self) -> str:
+        """
+        Returns a formatted string representation of the discipline configuration.
+
+        :return: A formatted string with all discipline details.
+        :rtype: str
+        """
         return f"""
         Discipline name: {self.name}
         Contol form: {self.contol_form}
